921591012988956b08ca13360a77536e281b30f0
caca <citravebriany>
2020-04-20 15:53:12 +0700

    Asolole

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import t from "tailwind-rn";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={t("h-full justify-center items-center")}>
+      <Text>Asolole</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
